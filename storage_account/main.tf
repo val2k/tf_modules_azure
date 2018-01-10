5ef81b0fbfe8edb54b6f52e9a6670325418bd52a
Valérian Kinyock <kinyock.va@gmail.com>
2018-01-10 11:26:35 +0100

    swap rscg and stoacc main.tf

--- storage_account/main.tf
+++ storage_account/main.tf
@@ -2,7 +2,3 @@ resource "random_id" "rdm_suffix" {
     byte_length = 3
 }
 
-resource "azurerm_resource_group" "resource_group" {
-    name = "${format("rq-%s-%s", var.base_name, random_id.random_suffix.hex)}"
-    location = "${var.location}"
-}
